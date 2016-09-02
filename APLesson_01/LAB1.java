21ceb1f2e97a30f3728b87309907468766b2b44b
mayarankupalli <mayarankupalli@gmail.com>
2016-09-02 13:41:23 -0700

    added files

---
+++ APLesson_01/LAB1.java
@@ -0,0 +1,10 @@
+public class LAB1
+{
+	public static void main(String[]args)
+	{
+		System.out.println ("><>");
+		System.out.println ("@( * O * )@");
+		
+		
+	}
+}
---
+++ APLesson_01/LAB1ASCII2.java
@@ -0,0 +1,10 @@
+public class LAB1ASCII2
+{
+	public static void main(String[]args)
+	{
+		System.out.println (" ,___, ");
+		System.out.println (" (O.o) ");
+		System.out.println (" /),,,,)");
+		System.out.println ("  \"\" ");
+	}
+}
