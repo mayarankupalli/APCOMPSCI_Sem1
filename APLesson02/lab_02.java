89ad68bfeda4d51f569a1a8e73e7ce3eb4c5fdbc
mayarankupalli <mayarankupalli@gmail.com>
2016-09-07 14:08:52 -0700

    Lab_02 and lesson_02

---
+++ APLesson02/lab_02.java
@@ -0,0 +1,33 @@
+public class lab_02
+{
+	public static void main(String[]args)
+	{
+		int x = 2;
+		int y = 4;
+		int answer = x * y;
+		
+		System.out.println(x + " multiplied by " + y + " is " + answer);
+		
+		String name = "Maya Rankupalli ";
+		String address = "666 elm street ";
+		String city = "san diego, CA ";
+		String zip = "92130";
+		
+		System.out.println(name + address + city + zip);
+		
+		int w = 5;
+		int l = 6;
+		int h = 7;
+		int rect = 2 * (w*l + l*h + h*w);
+		
+		System.out.println("the surface area of your rectangle is " + rect);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ APLesson02/lesson_02.java
@@ -0,0 +1,11 @@
+public class lesson_02
+{
+	public static void main(String[]args)
+	{
+		int l = 8;
+		int w = 5;
+		int recArea = l * w;
+		
+		System.out.println("the area if your rectangle is " + recArea);
+	}
+}
