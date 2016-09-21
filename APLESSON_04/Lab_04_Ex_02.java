38534319e8aac8fdf665722793e7832263401171
mayarankupalli <mayarankupalli@gmail.com>
2016-09-21 14:30:38 -0700

    lesson and lab 4 and chart

--- APLESSON_04/Lab_04_Ex_02.java
+++ APLESSON_04/Lab_04_Ex_02.java
@@ -4,6 +4,9 @@ public class Lab_04_Ex_02
 {
 	public static void main(String[]args)
 	{
+		Scanner kb = new Scanner(System.in);
+		Lab_04_Ex_02 form = new Lab_04_Ex_02();
+		
 		System.out.println("Enter your first name");
 		String firstname = kb.nextLine();
 		
@@ -21,5 +24,14 @@ public class Lab_04_Ex_02
 		
 		System.out.println("What is your subject");
 		String subject = kb.nextLine();
+		System.out.println("***************************");
+		form.format(site, year);
+		form.format(firstname, lastname);
+		form.format(title, subject);
+		System.out.println("***************************");
+	}
+	public void format(String site, String year)
+	{
+		System.out.printf("*%7s%15s*\n", site, year);
 	}
 }
