06b223b9a8adf6a429ce8ae41b43d7f950924e40
mayarankupalli <mayarankupalli@gmail.com>
2016-11-28 14:34:36 -0800

    10

---
+++ APLESSON_10/Lesson_10original.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Lesson_10original
+{
+	public static void main(String[]args)
+	{
+		Integer [] numbers = {1, 2, 3, 4, 5};
+		ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(numbers));
+		System.out.println(nums);
+		
+		
+	}
+}
